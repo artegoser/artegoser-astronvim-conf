c33ce5edbc2fee81241d6206c178c44142b607f4
Artemy Egorov <artegoser@gmail.com>
2023-08-22 16:14:28 +0300

    feat: fast load current dir session

--- init.lua
+++ init.lua
@@ -64,16 +64,4 @@ return {
       },
     },
   },
-
-  -- This function is run last and is a good place to configuring
-  -- augroups/autocommands and custom filetypes also this just pure lua so
-  -- anything that doesn't fit in the normal config locations above can go here
-  polish = function()
-    vim.api.nvim_create_autocmd("VimEnter", {
-      command = "set nornu nonu | Neotree toggle",
-    })
-    vim.api.nvim_create_autocmd("BufEnter", {
-      command = "set rnu nu",
-    })
-  end,
 }
